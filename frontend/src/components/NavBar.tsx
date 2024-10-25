4f4510399289b9d3adbfc6556e5920270dd8aaa4
miejejaw <mierafjejaw@gmail.com>
2024-10-25 11:02:42 +0300

    make nav bar fix and add logo

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,26 +1,41 @@
+import Image from "next/image";
 
-const titles = ['Home', 'About', 'Services','Project', 'Contact'];
-// Experience, Education, Skills
-
-const NavBar = () => {
-    return (
-        <nav className='p-4 flex justify-between'>
-            <h1 className='text-white text-2xl font-bold'>Mieraf</h1>
-
-            <ul className='flex space-x-6'>
-                {
-                    titles.map((title, index) => {
-                        return <li key={index} className='text-white'>{title}</li>
-                    }, [])
-                }
-            </ul>
-
-            {/*resume*/}
-            <div>
-                <button className='text-white px-4 py-2 border rounded'>Resume</button>
-            </div>
-        </nav>
-    )
+interface NavBarProps {
+    selected: string;
+    handleSelected: (title: string) => void;
+    navTitles: { title: string; handleScroll: () => void }[];
 }
+
+const NavBar = ({ selected, handleSelected, navTitles }: NavBarProps) => {
+    return (
+        <header className="fixed top-0 left-0 w-full h-16 bg-background py-2 px-16 shadow-lg z-10">
+            <nav className="flex justify-between items-center">
+                {/* Logo */}
+                <Image src="/logo.png" alt="logo" width={50} height={50} />
+
+                {/* Navigation items */}
+                <ul className="flex space-x-10">
+                    {navTitles.map((nav, index) => (
+                        <li
+                            key={index}
+                            className={`${selected === nav.title ? "text-navbarText" : "text-white"} hover:cursor-pointer hover:text-navbarText transition duration-300`}
+                            onClick={() => {
+                                handleSelected(nav.title);
+                                nav.handleScroll();
+                            }}
+                        >
+                            {nav.title}
+                        </li>
+                    ))}
+                </ul>
+
+                {/* Resume Button */}
+                <div className="border border-navbarText rounded transition duration-300 hover:bg-navbarText">
+                    <button className="text-white px-4 py-2">Resume</button>
+                </div>
+            </nav>
+        </header>
+    );
+};
 
 export default NavBar;
